b999a3082e0214a51de0a2c3c10557981b12cec2
clcser <clcser2021@udec.cl>
2022-07-16 22:54:49 -0400

    p2 y p4 funcionando

--- listado5/p2.h
+++ listado5/p2.h
@@ -1,6 +1,36 @@
+/*
+Claudia cser
+Solucion propia
+
+P2: Escriba la implementación de las siguientes funciones en lenguaje C:
+
+    crearFanaticos que recibe un número entero positivo y genera un vector dinámico conteniendo esa cantidad de 
+    objetos de tipo Fanatico generados aleatoriamente.
+
+    liberarFanaticos que recibe el puntero a un arreglo de Fanaticos y lo libera.
+
+    crearStaff que recibe varios números enteros, indicando cuántos miembros del staff se desea y genera un 
+    vector dinámico conteniendo esa cantidad de objetos de tipo Staff generados aleatoriamente.
+
+    liberarStaff que recibe el puntero a un arreglo de tipo Staff y lo libera.
+
+    crearJugadores que recibe varios números enteros, indicando cuántos jugadores de cada tipo se desea y genera
+    un vector dinámico conteniendo esa cantidad de objetos de tipo Jugador generados aleatoriamente.
+
+    liberarJugadores que recibe el puntero a un arreglo de tipo Jugador y lo libera.
+
+    crearEquipos que recibe un número entero, indicando cuántos equipos desea y genera un vector dinámico 
+    conteniendo esa cantidad de objetos de tipo Equipo generados aleatoriamente.
+
+    liberarEquipos que recibe el puntero a un arreglo de tipo Equipo y su dimensión y lo libera.
+*/
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+// esto es de la pregunta anterior //
 
 enum posicion {
     Arquero,
@@ -56,24 +86,28 @@ struct Equipo {
 
 //////////////////////// AQUI EMPIEZA ESTA PREGUNTA ///////////////////////////
 
+
 // crear vector de fanaticos aleatorios
 struct Fanatico *crearFanaticos(int numFanaticos) {
     struct Fanatico *vec = malloc(numFanaticos * sizeof(struct Fanatico));
-    char *nombres[] = {
-        "a",
-        "b",
-        "c",
-        "d",
-    };
     srand(time(NULL));
-
+    
+    char *nombreAl;
+    nombreAl = malloc(27);
+      
     for (int i = 0; i < numFanaticos; i++) { 
-        int rnd = rand() % sizeof(nombres); // crea un numero aleatorio entre 0 y la cantidad de nombres
+
+        int size = 2 + rand() % 25;
+        nombreAl[0] = 'A' + rand() % 25;
+        for(int i = 1; i < size; i++)
+            nombreAl[i] = 'a' + rand() % 25;
+        nombreAl[size] = '\0';
+
         struct Fanatico *fan = &vec[i];
 
-        strncpy(fan->nombre, nombres[rnd], 100);
-        fan->edad = rand() % 80 + 10; // numero aleatorio entre 10 y 89
-        fan->agresividad = rand() % 100; // numero aleatorio entre 0 y 99
+        strncpy(fan->nombre, nombreAl, 100);
+        fan->edad = rand() % 100; // numero aleatorio entre 0 y 99
+        fan->agresividad = rand() % 10000 / 100.0; // numero aleatorio entre 0 y 99
     }
     return vec;
 }
@@ -86,25 +120,26 @@ void liberarFanaticos(struct Fanatico *fans) {
 
 
 // crear vector de staff aleatorio
-struct Staff* crearStaff(int numDT, int numPrepArq, int numPrepFis, int numAsis, int numFisio, int numDoc) {
+struct Staff *crearStaff(int numDT, int numPrepArq, int numPrepFis, int numAsis, int numFisio, int numDoc) {
     int staffTot = numDT + numPrepArq + numPrepFis + numAsis + numFisio + numDoc; // cantidad total de de staff
     int staffEsp[6] = {numDT, numPrepArq, numPrepFis, numAsis, numFisio, numDoc}; // cantidad especifica de staff
     
     struct Staff *vec = malloc(staffTot * sizeof(struct Staff));
-    char *nombres[] = {
-        "a",
-        "b",
-        "c",
-        "d",
-    };
     
+    char *nombreAl;
+    nombreAl = malloc(27);
     int cont = 0;
+    
     for(int i = 0; i < staffTot; i++) {
+        int size = 2 + rand() % 25;
+        nombreAl[0] = 'A' + rand() % 25;
+        for(int i = 1; i < size; i++)
+            nombreAl[i] = 'a' + rand() % 25;
+        nombreAl[size] = '\0';
+    
         if(staffEsp[cont] > 0) {
-            int rnd = rand() % sizeof(nombres);
-            struct Staff *stf = &vec[i];
-
-            strncpy(stf->nombre, nombres[rnd], 100);
+           struct Staff *stf = &vec[i];
+            strncpy(stf->nombre, nombreAl, 100);
             stf->edad = rand() % 40 + 20;
             stf->rol = cont;
             stf->experiencia = rand() % 100;
@@ -129,28 +164,29 @@ struct Jugador *crearJugadores(int numArq, int numDef, int numCent, int numDel) 
     int jugEsp[4] = {numArq, numDef, numCent, numDel};
 
     struct Jugador *vec = malloc(jugTot * sizeof(struct Jugador));
-    char *nombres[] = {
-        "a",
-        "b",
-        "c",
-        "d",
-    };
 
     int cont = 0;
-    for(int i = 0; i < jugTot; i++) {
-        if(jugEsp[cont] > 0) {
-            int rnd = rand() %sizeof(nombres);
-            struct Jugador *jug = &vec[i];
+    char *nombreAl;
+    nombreAl = malloc(27);
+       
+    for(int i = 0; i < jugTot; i++) {     
+        int size = 2 + rand() % 25;
+        nombreAl[0] = 'A' + rand() % 25;
+        for(int i = 1; i < size; i++)
+            nombreAl[i] = 'a' + rand() % 25;
+        nombreAl[size] = '\0';
 
-            strncpy(jug->nombre, nombres[rnd], 100);
+        if(jugEsp[cont] > 0) {
+            struct Jugador *jug = &vec[i];
+            strncpy(jug->nombre, nombreAl, 100);
             jug->edad = rand() % 30 + 12;
             jug->posicion = cont;
-            jug->regate  = rand() % 100;
-            jug->defensa = rand() % 100;
-            jug->reflejos = rand() % 100;
-            jug->velocidad = rand() % 100;
-            jug->dureza = rand() % 100;
-            jug->resistencia = rand() % 100;
+            jug->regate  = rand() % 10000 / 100.0;
+            jug->defensa = rand() % 10000 / 100.0;
+            jug->reflejos = rand() % 10000 / 100.0;
+            jug->velocidad = rand() % 10000 / 100.0;
+            jug->dureza = rand() % 10000 / 100.0;
+            jug->resistencia = rand() % 10000 / 100.0;
 
             jugEsp[cont]--;
         }
@@ -169,29 +205,34 @@ void liberarJugadores(struct Jugador *jugadores) {
 // crear vector de equipos aleatorios
 struct Equipo *crearEquipos(int numEquipos) {
     struct Equipo *vec = malloc(numEquipos * sizeof(struct Equipo));
-    char *nombres[] = {
-        "a",
-        "b",
-        "c",
-        "d",
-    };
-    srand(time(NULL));
 
+    srand(time(NULL));
     for (int i = 0; i < numEquipos; i++) { 
-        int rnd = rand() % sizeof(nombres); // crea un numero aleatorio entre 0 y la cantidad de nombres
+        char *equipoAl;
+        equipoAl = malloc(27);
+    
+        int size = 2 + rand() % 25;
+        equipoAl[0] = 'A' + rand() % 25;
+        for(int i = 1; i < size; i++)
+            equipoAl[i] = 'a' + rand() % 25;
+        equipoAl[size] = '\0';
+
         struct Equipo *eq = &vec[i];
 
-        strncpy(eq->nombre, nombres[rnd], 100);
+        strncpy(eq->nombre, equipoAl, 100);
         eq->anyoFundacion = rand() % 123 + 1900; // año aleatorio entre 1900 y 2022
+        
         for(int i = 0; i < 6; i++) {
-            eq->numStaff[i] = rand() % 2 + 1; // numero aleatorio de cada tipo de staff entre 1 y 3
+            eq->numStaff[i] = rand() % 3 + 1; // numero aleatorio de cada tipo de staff entre 1 y 3
         }
         eq->staff = crearStaff(eq->numStaff[0], eq->numStaff[1], eq->numStaff[2], eq->numStaff[3], eq->numStaff[4], eq->numStaff[5]);
+        
         for(int i = 0; i < 4; i++) {
-            eq->numJugadores[i] = rand() % 3 + 2; // numero aleatorio de cada tipo de jugador entre 2 y 5
+            eq->numJugadores[i] = rand() % 4 + 2; // numero aleatorio de cada tipo de jugador entre 2 y 5
         }
         eq->jugadores = crearJugadores(eq->numJugadores[0], eq->numJugadores[1], eq->numJugadores[2], eq->numJugadores[3]);
-        eq->numFanaticos = rand() % 10000; // fanaticos aleatorios entre 0 y 10 mil
+        
+        eq->numFanaticos = rand() % 50; // fanaticos aleatorios entre 0 y 50
         eq->fanaticos = crearFanaticos(eq->numFanaticos);
     }
     return vec;
@@ -202,4 +243,20 @@ void liberarEquipos(struct Equipo *equipos, int numEquipos) { // REVISAR ESTO!
     free(equipos);
 }
 
+/*
+int main() {
+    int numEquipos;
+    scanf("%d", &numEquipos);
+    struct Equipo *equipo;
+    
+    equipo = crearEquipos(numEquipos);
 
+    for(int i = 0; i < numEquipos; i++) {
+        printf("Nombre: %s\n",equipo[i].nombre);
+    }
+
+    liberarEquipos(equipo, numEquipos);
+
+    return 0;
+}
+*/
---
+++ listado5/p4.c
@@ -0,0 +1,124 @@
+/*
+Claudia Cser
+solucion propia
+
+Escriba una función llamada imprimiDB que reciba un puntero a un vector de Equipos e imprima toda la información
+de los equipos por pantalla.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "p2.h"
+
+void imprimirFanaticos(struct Fanatico *fanatico, int *numFanaticos) {
+    for(int i = 0; i < *numFanaticos; i++) {
+        printf("-Nombre: %s\n", fanatico[i].nombre);
+        printf("    -Edad: %d\n", fanatico[i].edad);
+        printf("    -Agresividad: %.3f\n", fanatico[i].agresividad);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void imprimirJugadores(struct Jugador *jugador, int *numJugadores) {
+    for(int i = 0; i < 4; i++) {
+        int numActual = numJugadores[i];
+        for(int j = 0; j < numActual; j++) {
+            printf("-Nombre: %s\n", jugador[j].nombre);
+            printf("    -Edad: %d\n", jugador[j].edad);
+            
+            switch(i) {
+                case 0:
+                    printf("    -Posicion: Arquero\n");
+                    break;
+                case 1:
+                    printf("    -Posicion: Defensor\n");
+                    break;
+                case 2:
+                    printf("    -Posicion: Medio\n");
+                    break;
+                case 3:
+                    printf("    -Posicion: Delantero\n");
+                    break;
+            }
+
+            printf("    -Regate: %.3f \n", jugador[j].regate);
+            printf("    -Defensa: %.3f \n", jugador[j].defensa);
+            printf("    -Reflejos: %.3f \n", jugador[j].reflejos);
+            printf("    -Velocidad: %.3f \n", jugador[j].velocidad);
+            printf("    -Dureza: %.3f \n", jugador[j].dureza);
+            printf("    -Resistencia: %.3f \n", jugador[j].resistencia);
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+void imprimirStaff(struct Staff *staff, int *numStaff) {
+    for(int i = 0; i < 6; i++) {
+        int numActual = numStaff[i];
+        for(int j = 0; j < numActual; j++) {
+            printf("-Nombre: %s\n", staff[j].nombre);
+            printf("    -Edad: %d\n", staff[j].edad);
+            
+            switch(i) {
+                case 0:
+                    printf("    -Rol: DT\n");
+                    break;
+                case 1:
+                    printf("    -Rol: Preparadr de arqueros\n");
+                    break;
+                case 2:
+                    printf("    -Rol: Preparador fisico\n");
+                    break;
+                case 3:
+                    printf("    -Rol: Asistente\n");
+                    break;
+                case 4:
+                    printf("    -Rol: Fisioterapeuta\n");
+                    break;
+                case 5:
+                    printf("    -Rol: Doctor\n");
+                    break;
+            }
+
+            printf("    -Experiencia: %d/100\n", staff[j].experiencia);
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+void imprimirEquipo(struct Equipo *equipo) {
+    printf("Nombre: %s\n\n", equipo->nombre);
+    printf("Año de fundacion: %d\n\n", equipo->anyoFundacion);
+    printf("STAFF:\n\n");
+    imprimirStaff(equipo->staff, equipo->numStaff);
+    printf("JUGADORES:\n\n");
+    imprimirJugadores(equipo->jugadores, &equipo->numJugadores[0]);
+    printf("FANATICOS:\n\n");
+    imprimirFanaticos(equipo->fanaticos, &equipo->numFanaticos);
+}
+
+void imprimiDB(struct Equipo *equipo, int numEquipos) {
+    for(int i = 0; i < numEquipos; i++) {
+        printf("-------------------------Equipo n°%d-------------------------\n", i+1);
+        imprimirEquipo(&equipo[i]);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+    int numEquipos;
+    struct Equipo *equipos;
+
+    scanf("%d",&numEquipos);
+    equipos = crearEquipos(numEquipos);
+
+    imprimiDB(equipos, numEquipos);
+
+    liberarEquipos(equipos, numEquipos);
+    
+    return 0;
+}
